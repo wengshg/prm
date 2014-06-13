812008aad6cd95b03a52fc47e457ded26175de0e
Victor Weng <shweng@cisco.com>
2014-06-13 17:53:41 +0800

    small tool to support audo build & deploy to tomcat.

---
+++ tomcatDeploy.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# This script requires:
+# 1. set the variable of CATALINA_HOME
+# 2. set path to CATALIHA bin.
+#
+gradle -b build.war.gradle clean build
+shutdown.sh
+rm -rf ${CATALINA_HOME}/webapps/prm*
+cp build/libs/prm*.war ${CATALINA_HOME}/webapps/prm.war
+startup.sh
