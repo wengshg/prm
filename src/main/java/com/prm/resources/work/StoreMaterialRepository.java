01212602d415326aebe921ee9f8eb864dcc27104
Victor Weng <shweng@cisco.com>
2014-06-13 18:43:19 +0800

    1. Add a search for store material by roomid.

--- src/main/java/com/prm/resources/work/StoreMaterialRepository.java
+++ src/main/java/com/prm/resources/work/StoreMaterialRepository.java
@@ -1,5 +1,8 @@
 package com.prm.resources.work;
 
+import java.util.List;
+
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.prm.models.work.StoreMaterial;
@@ -8,5 +11,11 @@ import com.prm.resources.PrmRepository;
 @RepositoryRestResource(collectionResourceRel = "storematerial", path = "storematerial")
 public interface StoreMaterialRepository extends
 		PrmRepository<StoreMaterial, Long> {
-
+	
+	/**
+	 * Search by roomid and order by signed date in descend.
+	 * @param rid
+	 * @return
+	 */
+	List<StoreMaterial> findByRidOrderBySignedDateDesc(@Param("rid") Long rid);
 }
--- tomcatDeploy.bash
+++ tomcatDeploy.bash
@@ -6,6 +6,7 @@
 #
 gradle -b build.war.gradle clean build
 shutdown.sh
+sleep 5s
 rm -rf ${CATALINA_HOME}/webapps/prm*
 cp build/libs/prm*.war ${CATALINA_HOME}/webapps/prm.war
 startup.sh
